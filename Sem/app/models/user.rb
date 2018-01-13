e23be7fb0dead27f058123cc414b471c3a1b0394
Alina575 <alinchick575@gmail.com>
2018-01-14 00:00:34 +0300

    Admin module added

--- Sem/app/models/user.rb
+++ Sem/app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            format: {
+              with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+            }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :tasks
--- Sem/config/routes.rb
+++ Sem/config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '//admin', as: 'rails_admin'
   resources :comments
   resources :tasks
   resources :projects
   devise_for :users, path: 'users', path_names: { sign_in: 'sign_in', sign_out: 'sign_out', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
   devise_for :models
 
-  root 'pages#welcome'  
+  get 'sign_out', to: 'sessions#destroy', as: 'sign_out'
+  resources :users
 
+  root 'pages#welcome'  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
